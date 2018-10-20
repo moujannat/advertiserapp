6fa38d78af5ab27bd36795c099ce098f216cb839
Mou <jannatul.mou202@gmail.com>
2018-10-20 16:28:32 -0700

    Initial Generated Spring Project

---
+++ src/main/java/com/spring/projects/advertiserapp/AdvertiserappApplication.java
@@ -0,0 +1,12 @@
+package com.spring.projects.advertiserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertiserappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertiserappApplication.class, args);
+	}
+}
